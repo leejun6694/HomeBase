1e38365b07a58e191d9f72d0a540e52288e7757b
JUNLEE <leejun6694@gmail.com>
2017-08-05 18:38:10 +0900

    add HomeBase project

---
+++ HomeBase/HomeBase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HomeBase
+//
+//  Created by JUN LEE on 2017. 8. 5..
+//  Copyright © 2017년 LemonKooma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ HomeBase/HomeBaseTests/HomeBaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  HomeBaseTests.swift
+//  HomeBaseTests
+//
+//  Created by JUN LEE on 2017. 8. 5..
+//  Copyright © 2017년 LemonKooma. All rights reserved.
+//
+
+import XCTest
+@testable import HomeBase
+
+class HomeBaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
