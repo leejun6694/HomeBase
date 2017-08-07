afa2ad17c00bfc073a2e82d542dcef6a64e972bb
JUNLEE <leejun6694@gmail.com>
2017-08-07 15:01:26 +0900

    separate tab item storyboard reference

---
+++ HomeBase/HomeBase/TeamMainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TeamMainViewController.swift
+//  HomeBase
+//
+//  Created by JUN LEE on 2017. 8. 7..
+//  Copyright © 2017년 LemonKooma. All rights reserved.
+//
+
+import UIKit
+
+class TeamMainViewController: UIViewController {
+    
+}
