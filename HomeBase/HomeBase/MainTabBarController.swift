6e08ad7af1f769fc4afecb642f733bc8d5c5480e
JUNLEE <leejun6694@gmail.com>
2017-08-09 04:00:34 +0900

    complete AddScheduleViewController

--- HomeBase/HomeBase/MainTabBarController.swift
+++ HomeBase/HomeBase/MainTabBarController.swift
@@ -15,7 +15,6 @@ class MainTabBarController: UITabBarController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        
         if first == false {
             first = true
             performSegue(withIdentifier: "segueTabToStart", sender: self)
