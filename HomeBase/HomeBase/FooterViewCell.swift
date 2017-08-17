4f34f26a1f6fc86ddb30377d3c811b9cb57282e5
yang hee jung <puuurm@naver.com>
2017-08-17 15:32:54 +0900

    add customizing table view in team main page

---
+++ HomeBase/HomeBase/FooterViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FooterViewCell.swift
+//  HomeBase
+//
+//  Created by yangpc on 2017. 8. 17..
+//  Copyright © 2017년 LemonKooma. All rights reserved.
+//
+
+import UIKit
+
+class FooterViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HomeBase/HomeBase/HeaderViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderViewCell.swift
+//  HomeBase
+//
+//  Created by yangpc on 2017. 8. 17..
+//  Copyright © 2017년 LemonKooma. All rights reserved.
+//
+
+import UIKit
+
+class HeaderViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
